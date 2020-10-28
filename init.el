2afbcfe100846c64896cf53f81dfd2acdc3665fb
Jake Doliskani <jake.doliskani@gmail.com>
2020-10-28 12:34:21 -0400

    Add an unfill-region function

--- init.el
+++ init.el
@@ -300,4 +300,19 @@
 
 
 
+
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;  
+;; Custom functions
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(defun unfill-region (beg end)
+  "Unfill the region, joining text paragraphs into a single
+    logical line.  This is useful, e.g., for use with
+    `visual-line-mode'."
+  (interactive "*r")
+  (let ((fill-column (point-max)))
+    (fill-region beg end)))
+
+
 ;;; init ends here
